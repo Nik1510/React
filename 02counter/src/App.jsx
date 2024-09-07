675acf558ad067d2df2d851c0a9888cadf682121
Nik1510 <Nikhilshaw575@gmail.com>
2024-09-08 00:10:45 +0530

    video 8

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,7 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-//  Q.) I have to set up counter which is cannot go less than 0 and greater than 20 
 
   let [counter,setCounter] = useState(0) // there is a variable called "counter" and 
   // there is setCounter  which is responsible for update "counter"
@@ -12,29 +11,45 @@ function App() {
 
   // let counter = 5;
   
+//  Q.) I have to set up counter which is cannot go less than 0 and greater than 20 
 
-  const addValue =() =>{
-    console.log("Clicked "+counter)
-    if(counter<=20){
-    // counter = counter+1; now we don't need to use this as we are using hooks such as useStates 
-    counter = counter +1
-    setCounter(counter)
-    }else{
-      console.log("Counter is greater than 20")
-    }
-  }
+ // Answer :-
+  // const addValue =() =>{
+  //   console.log("Clicked "+counter)
+  //   if(counter<=20){
+  //   // counter = counter+1; now we don't need to use this as we are using hooks such as useStates 
+  //   counter = counter +1
+  //   setCounter(counter)
+  //   }else{
+  //     console.log("Counter is greater than 20")
+  //   }
+  // }
   
-  const remValue = ()=>{
-    if(counter>0){
-    console.log("Clicked "+counter)
-    // counter = counter-1;
-    setCounter(counter-1)
-    }else{
-      console.log("Counter is less than 0")
+  // const remValue = ()=>{
+  //   if(counter>0){
+  //   console.log("Clicked "+counter)
+  //   // counter = counter-1;
+  //   setCounter(counter-1)
+  //   }else{
+  //     console.log("Counter is less than 0")
 
+  //   }
+  // }
+
+
+  // this holds very important concept video number 8
+  const addValue =() =>{
+      console.log("Clicked "+counter)
+      setCounter((prevCount) => prevCount + 1)
+      setCounter((prevCount) => prevCount + 1)
+      setCounter((prevCount) => prevCount + 1)
+      setCounter((prevCount) => prevCount + 1)
     }
+   
+  const remValue =() =>{
+    console.log("Clicked "+counter)
+    setCounter(counter-1)
   }
-
   return (
     <>
      <h1> Chai aur React</h1>
