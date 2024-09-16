e073181faa028e062bddf34eb7250b73c43c46ed
Nikhil Kumar <nikhilshaw575@gmail.com>
2024-09-17 01:04:49 +0530

    change

--- 07_react_router/src/App.jsx
+++ 07_react_router/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-import Footer from './components/Footer/footer'
+// import Footer from './components/Footer/footer'
 
 function App() {
   const [count, setCount] = useState(0)
